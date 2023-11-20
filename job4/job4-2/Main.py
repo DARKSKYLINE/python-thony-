d48437834a5272ecc5369d4fab99b1cd3ce979db
SUPAROEK <hopehut8826@gmail.com>
2023-11-20 13:56:00 +0700

    Update

--- job4/job4-2/Main.py
+++ job4/job4-2/Main.py
@@ -1,15 +1,3 @@
-###############################################################
-# SSD1306 OLED Display I2C Tests with the Raspberry Pi Pico
-# -- ADC Reading and Display of MEMS Microphone
-#
-# by Joshua Hrisko, Maker Portal LLC (c) 2021
-#
-#
-# Based on the Pico Micropython repository at:
-# https://github.com/raspberrypi/pico-micropython-examples/tree/master/i2c/1306oled
-###############################################################
-#
-#
 from machine import Pin, I2C, ADC
 from ssd1306 import SSD1306_I2C
 import framebuf,sys,time
@@ -36,22 +24,12 @@ oled = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev) # oled controller
 #oled.write_cmd(0xc0) # flip display to place 0,0 at lower-left corner
 adc_2 = machine.ADC(2) # ADC channel 2 for input
 
-
- 
 while True:
     oled.fill(0) # clear the display
-    #for jj in range(pix_res_x): # loop through horizontal display resolution
-        #adc_pt = adc_2.read_u16() # read adc and get 16-bit data point
-        #plot_pt = (adc_pt/((2**16)-1))*(pix_res_y-1) # convert to OLED pixels
-        #oled.text('.',jj,int(plot_pt)) # update x=jj with data point
- #   oled.text('AUTISTIC',35,30)
-#    oled.show() # show the new text and image
-    #oled.text("Temperature: {0}°C Humidity: {1}% ".format(temp, hum))
-
     sensor.measure()
     temp = sensor.temperature() 
     hum = sensor.humidity()
-    oled.text('Temp: {0}C '.format(temp, hum),0,0)
+    oled.text('Temp: {0} C '.format(temp, hum),0,0)
     oled.text('Hum: {1}% '.format(temp, hum),0,20)
     oled.show() # show the new text and image
     sleep(1)
